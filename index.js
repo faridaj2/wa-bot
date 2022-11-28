def49570f74651d632fcc087be33d5091be75834
faridaj2 <ajfarid6@gmail.com>
2022-11-28 09:12:45 +0700

    third commit

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-const { Client } = require('whatsapp-web.js');
+const { Client } = require("whatsapp-web.js");
 const client = new Client();
-const qrcode = require('qrcode-terminal');
+const qrcode = require("qrcode-terminal");
 
-client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
-    qrcode.generate(qr, {small: true});
+client.on("qr", (qr) => {
+  console.log("QR RECEIVED", qr);
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
 
-client.on('message', message => {
-	console.log(message.body);
+client.on("message", (message) => {
 });
 
-client.on('message', message => {
-	if(message.body === 'assalamualaikum') {
-		client.sendMessage(message.from, 'waalaikum salam');
-	}
-    client.on('message', message => {
-        if(message.body === 'alamat') {
-            client.sendMessage(message.from, 'krajan Jalan Raden Wijaya No, Jl. R Supono, Jalen I, Setail, Kec. Genteng, Kabupaten Banyuwangi, Jawa Timur 68486');
-        }
-    });
-    client.on('message', message => {
-        if(message.body === 'ig') {
-            client.sendMessage(message.from, 'Kamu bisa menemukan instagram kami di @dartsachannel');
-        }
-    });
+client.on("message", (message) => {
+  if (message.body === "assalamualaikum") {
+    client.sendMessage(message.from, "waalaikum salam");
+  }
+
+  if (message.body === "ig") {
+    client.sendMessage(
+      message.from,
+      "Kamu bisa menemukan instagram kami di @dartsachannel"
+    );
+  }
+  if (msg.body == 'alamat') {
+    msg.reply('krajan Jalan Raden Wijaya No, Jl. R Supono, Jalen I, Setail, Kec. Genteng, Kabupaten Banyuwangi, Jawa Timur 68486');
+  }
+
 });
- 
+
 client.initialize();
- 
